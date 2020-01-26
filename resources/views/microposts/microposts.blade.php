846f962de8b3cfd3fecb98bdb71b800d3c426fc4
akiyoshi.takazawa <akiyoshi.t3@gmail.com>
2020-01-26 20:11:30 +0000

    favorites

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -2,21 +2,21 @@
     @foreach ($microposts as $micropost)
         <li class="media mb-3">
             <img class="mr-2 rounded" src="{{ Gravatar::src($micropost->user->email, 50) }}" alt="">
-            <div class="media-body">
-                <div>
-                    {!! link_to_route('users.show', $micropost->user->name, ['id' => $micropost->user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
-                </div>
-                <div>
-                    <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
-                </div>
-                <div>
-                    @if (Auth::id() == $micropost->user_id)
-                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
-                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
-                        {!! Form::close() !!}
-                    @endif
-                    @include('user_favorite.favorite_button')
-                </div>
+            <div class="media-body mb-9">
+                    <div>
+                        {!! link_to_route('users.show', $micropost->user->name, ['id' => $micropost->user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
+                    </div>
+                    <div>
+                        <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                    </div>
+                    <div class="d-flex justify-content-start">
+                        @if (Auth::id() == $micropost->user_id)
+                            {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                            {!! Form::close() !!}
+                        @endif
+                        @include('user_favorite.favorite_button', ['microposts' => $microposts])
+                    </div>
             </div>
         </li>
     @endforeach
